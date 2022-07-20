a1ce04673a3c96890f93a4010fde46387a870308
ZombieFly <xyzomfly@gmail.com>
2022-07-20 18:49:01 +0800

    🐞 fix: 版本显示错误

--- BedrockFakeMotd.py
+++ BedrockFakeMotd.py
@@ -1,8 +1,13 @@
+"""
+基于
+https://github.com/xBoyMinemc/dreamTool/blob/main/motd/fakeMotdpe.js
+的python实现
+"""
+
 import logging
 
 key_start = '0100000000'
-motd_start = '1c000000006d2ac11d806c7ed9eec97c1f00\
-    ffff00fefefefefdfdfdfd12345678005d'
+motd_start = '1c000000006d2ac11d806c7ed9eec97c1f0'
 
 motd_array = [
     "MCPE",
@@ -15,7 +20,7 @@ motd_array = [
     "Test For IC mod in PE",
     "S",
     "1",
-    "15555",
+    "19132",
     "6661",
     ""
 ]
@@ -28,5 +33,4 @@ def summ_motd(data):
         for element in motd_array
     )
     out = motd_start + data
-    print(type(out))
     return bytes(out, encoding='utf_8')
--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ async def run_server():
         server.sendto(handle_client(data), addr)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('127.0.0.1', 15555))
+server.bind(('127.0.0.1', 19132))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run_server())
